a58b432d91add358ad322227d8c948220e6478a7
Mauricio Viana <mau_viana@live.com>
2020-08-06 22:01:35 -0300

    Projeto inicial

---
+++ src/main/java/br/com/api/ComicsApplication.java
@@ -0,0 +1,13 @@
+package br.com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicsApplication.class, args);
+	}
+
+}
